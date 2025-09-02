b307c523164070e9e5f79ab78359cba4b7a2f5e8
kattaNagasainikhila-wq <kattasainikhila@gmail.com>
2025-09-02 15:38:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 # Raw GitHub URLs of your files
 DISEASES_URL = "https://github.com/kattaNagasainikhila-wq/health_data/blob/main/diseases_data.json"
-SYMPTOMS_URL = 
+SYMPTOMS_URL = "https://github.com/kattaNagasainikhila-wq/health_data/blob/main/preventions_data.json"
 PREVENTIONS_URL = 
 # Function to load JSON from GitHub
 def load_json(url):
