a34137c72412def70b452ad5e7a56645b2ecf78d
kattaNagasainikhila-wq <kattasainikhila@gmail.com>
2025-09-04 10:59:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 # ---------- GITHUB RAW FILES ----------
 DISEASES_URL = "https://raw.githubusercontent.com/kattaNagasainikhila-wq/health_data/main/diseases.json"
 SYMPTOMS_URL = "https://raw.githubusercontent.com/kattaNagasainikhila-wq/health_data/main/symptoms.json"
-PREVENTIONS_URL = 
+PREVENTIONS_URL ="https://raw.githubusercontent.com/kattaNagasainikhila-wq/health_data/main/preventions.json"
 # Cache for GitHub JSON to avoid fetching every time
 data_cache = {}
 
